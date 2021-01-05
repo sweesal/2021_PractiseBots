2a718ce8a0f5aa1cfb54bead1311c627eb971628
tianyan <njsweesal@gmail.com>
2021-01-05 15:14:08 +0800

    modified config botC 2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleDriverBotC.java
@@ -0,0 +1,85 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.robotC.subsystems.DriveTrainC;
+import org.firstinspires.ftc.teamcode.robotC.subsystems.IntakeC;
+import org.firstinspires.ftc.teamcode.robotC.subsystems.ShooterC;
+import org.firstinspires.ftc.teamcode.robotC.RobotMapBotC;
+
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Single Driver Mode C", group="C")
+//@Disabled
+
+public class SingleDriverBotC extends OpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    RobotMapBotC robotMapBotC = new RobotMapBotC();
+    private DriveTrainC driveTrain;
+    private IntakeC intake;
+    private ShooterC shooter;
+
+    @Override
+    public void init() {
+        robotMapBotC.robotInit(hardwareMap);
+        driveTrain = new DriveTrainC();
+        intake = new IntakeC();
+        shooter = new ShooterC();
+        telemetry.addData("Status", "Initialized");
+    }
+
+    @Override
+    public void loop() {
+        // DriveTrain.
+        driveTrain.driveMecanum(
+                -gamepad1.left_stick_y*0.99, -gamepad1.right_stick_x*0.99, -gamepad1.left_stick_x*0.99, gamepad1.x);
+
+        // Superstructure
+        intake.setIntake(gamepad1.x, gamepad1.b);
+        shooter.setShooter(gamepad1.left_bumper);
+        shooter.setTrigger(gamepad1.right_bumper);
+        shooter.setElevator(gamepad1.right_stick_y*0.6, false, false);
+
+        // This is for showing the encoder & switch value of the elevator.
+        telemetry.addData("Elevator Position", "%5.2f", shooter.getElevator());
+
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotC/subsystems/ShooterC.java
@@ -0,0 +1,103 @@
+package org.firstinspires.ftc.teamcode.robotC.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.robotB.RobotMapBotB;
+import org.firstinspires.ftc.teamcode.robotC.RobotMapBotC;
+
+public class ShooterC {
+
+    private final DcMotor shooter = RobotMapBotC.shooter;
+    private final DcMotor elevator = RobotMapBotC.elevator;
+    private final Servo trigger = RobotMapBotC.trigger;
+    private final ElapsedTime triggerTimer;
+    private final ElapsedTime slopeTimer;
+
+    private boolean isShoot = false;
+    private static boolean isTriggered = false;
+    private static double slopeAngle = 0.55; //default value
+
+    public ShooterC() {
+        shooter.setDirection(DcMotorSimple.Direction.FORWARD);
+        shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        elevator.resetDeviceConfigurationForOpMode();
+        elevator.setDirection(DcMotorSimple.Direction.FORWARD);
+        elevator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        trigger.setDirection(Servo.Direction.FORWARD);
+        triggerTimer = new ElapsedTime();
+        slopeTimer = new ElapsedTime();
+    }
+
+//    public void setShooter (boolean isBtnPressed) {
+//        if (isBtnPressed)
+//            isShooting = !isShooting;
+//        if (isShooting)
+//            shooter.setPower(0.99);//shooting power
+//        else
+//            shooter.setPower(0);//stop shooting
+//    }
+
+    public void setShooter (boolean isBtnPressed) {
+        if(isBtnPressed && slopeTimer.seconds() > 0.2) {
+            isShoot = !isShoot;
+            slopeTimer.reset();
+        }
+        if(isShoot) shooter.setPower(0.99);
+        else shooter.setPower(0);
+    }
+
+    // Set trigger.
+    public void setTrigger (boolean btnSet) {
+        if(btnSet && triggerTimer.seconds() > 0.3) {
+            isTriggered = true;
+            triggerTimer.reset();
+            slopeTimer.reset();
+        }
+        if(isTriggered) {
+            trigger.setPosition(0.5);
+            if(triggerTimer.seconds() > 1.0)
+            {
+                trigger.setPosition(0);
+                isTriggered = false;
+            }
+        }
+    }
+
+
+    // Elevator mechanism can only move to one direction if top/button limit was triggered.
+    public void setElevator (double input, boolean isAtTop, boolean isAtButton) {
+        if (isAtTop)
+            elevator.setPower(Range.clip(input, -0.2, 0));
+        else if (isAtButton)
+            elevator.setPower(Range.clip(input, 0, 0.2));
+        else
+            elevator.setPower(Range.clip(input, -0.5, 0.5));
+    }
+
+    // When Y btn of gamepad was pressed elevator goes up, and goes down if A btn pressed.
+    // The elevator will single-directional-maneuverable when top/button limit was triggered.
+    public void setElevator (boolean cmdUp, boolean cmdDown, boolean isAtTop, boolean isAtButton) {
+        if (isAtTop)
+            elevator.setPower(cmdDown ? -0.25 : 0);
+        else if (isAtButton)
+            elevator.setPower(cmdUp ? 0.25 : 0);
+        else if (!isAtTop | !isAtButton){
+            if (cmdUp)
+                elevator.setPower(0.3);
+            if (cmdDown)
+                elevator.setPower(-0.25);
+        } else if (!cmdDown && !cmdUp){
+            elevator.setPower(0);
+        }
+    }
+
+    public double getElevator () {
+        return elevator.getCurrentPosition();
+    }
+
+}
