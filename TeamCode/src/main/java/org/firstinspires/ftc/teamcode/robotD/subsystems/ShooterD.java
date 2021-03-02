b008564b277d4b99a8e4d3078d0998866d462e4b
Ian Tian <sweesal@sina.com>
2021-03-02 12:34:03 +0800

    add shooter for robotD

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotD/subsystems/ShooterD.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotD/subsystems/ShooterD.java
@@ -15,15 +15,17 @@ public class ShooterD {
     private double kSpeedingUpDelayTime = 3;
     private double kShootDelayTime = 1;
 
+    private double currentStateStartTime = 0;
+
     private final DcMotor shooter = RobotMapBotD.shooter;
     private final Servo trigger = RobotMapBotD.trigger;
 
     private static boolean isShooting = false; //default state;
+    private ElapsedTime shootingChangeTimer = new ElapsedTime();
 
     private enum SHOOTER_STATE {
         IDLE, SPEEDING_UP, FULL_POWER, MANUAL
     }
-
 
     private SHOOTER_STATE shooterState = SHOOTER_STATE.IDLE;
 
@@ -42,17 +44,98 @@ public class ShooterD {
     }
 
     private void updateShooter (double timeStamp) {
+        SHOOTER_STATE newState = shooterState;
+        double currentTime = getTimer();
+        double timeInState = currentTime - currentStateStartTime;
 
+        // State transition
+        switch (shooterState) {
+            case IDLE:
+                newState = handleIdleTransition(timeInState);
+                break;
+            case SPEEDING_UP:
+                newState = handleSpeedingUpTransition (timeInState, shooterState);
+                break;
+            case FULL_POWER:
+                newState = handleFullPowerTransition (timeInState, shooterState);
+                break;
+            case MANUAL:
+                newState = handleManualTransition (); // btn ctrl
+                break;
+            default:
+                System.out.println("Unexpected shooter state: " + shooterState);
+                newState = shooterState;
+                break;
+        }
 
+        if (newState != shooterState) {
+            System.out.println(currentTime + ": Changed state: " + shooterState + " -> " + newState);
+            shooterState = newState;
+            currentStateStartTime = currentTime;
+            timeInState = 0.0;
+        }
+
+        // State action
         switch (shooterState) {
             case IDLE:
                 setShooterFree(0);
+                break;
             case SPEEDING_UP:
-
+                speedingUpShooter(timeInState);
+                break;
             case FULL_POWER:
-
-
+                setShooterFree(0.99);
+                break;
+            case MANUAL:
+                break;
+            default:
+                System.out.println("Unexpected gear grabber system state: " + shooterState);
+                break;
         }
+    }
+
+    private SHOOTER_STATE handleManualTransition() {
+        return SHOOTER_STATE.MANUAL;
+    }
+
+    private SHOOTER_STATE handleIdleTransition(double timeInState) {
+        return SHOOTER_STATE.IDLE;
+    }
+
+    private SHOOTER_STATE handleSpeedingUpTransition(double timeInState, SHOOTER_STATE shooterState) {
+        switch (shooterState) {
+            case IDLE:
+            case SPEEDING_UP:
+                return SHOOTER_STATE.SPEEDING_UP;
+            case FULL_POWER:
+                if (timeInState > kShootDelayTime)
+                    return SHOOTER_STATE.FULL_POWER;
+                return SHOOTER_STATE.SPEEDING_UP;
+        }
+        return SHOOTER_STATE.SPEEDING_UP;
+    }
+
+    private SHOOTER_STATE handleFullPowerTransition(double timeInState, SHOOTER_STATE shooterState) {
+        switch (shooterState) {
+            case IDLE:
+                if (timeInState > kSpeedingUpDelayTime)
+                    return SHOOTER_STATE.IDLE;
+                return SHOOTER_STATE.FULL_POWER;
+            case SPEEDING_UP:
+                if (timeInState > kShootDelayTime)
+                    return SHOOTER_STATE.FULL_POWER;
+                return SHOOTER_STATE.SPEEDING_UP;
+            case FULL_POWER:
+                return SHOOTER_STATE.FULL_POWER;
+            case MANUAL:
+                return SHOOTER_STATE.MANUAL;
+        }
+
+        return SHOOTER_STATE.FULL_POWER;
+    }
+
+
+    private void speedingUpShooter(double timeInState) {
     }
 
     private double getTimer () {
